60bec57f61a939abfd29049c18bb5223bc2db92b
dogghoodie <dogghoodie@gmail.com>
2025-06-10 04:57:57 -0500

    Created game skeleton.

---
+++ pkg/game/game.go
@@ -0,0 +1,21 @@
+package game
+
+type Stats struct {
+	streak  int
+	avgTime float64
+	bestKey byte
+}
+
+// TODO : Build game logic and determine if need move to gui
+func Start() error {
+	// NOTE : GAME LOGIC >>
+	// while loop
+	// check input
+	// if char == target : continue next char, streak++
+	// if char != target : break
+	// prompt continue y/n
+
+	// NOTE : HARD MODE >>
+	// reflex view moves to random position each char
+	return nil
+}
